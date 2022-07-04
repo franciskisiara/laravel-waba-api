869de384f3e90380f9cb14ccac689fe3c77454ef
franciskisiara <franciskisiara@gmail.com>
2022-07-04 22:36:51 +0300

    hodl this up and do nothing else

---
+++ database/migrations/2022_07_04_181221_create_apartments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('caretaker_id')->constrained('users')->onDelete('cascade');
+            $table->integer('flat_rate_limit')->default(0);
+            $table->decimal('flat_rate', 4, 2)->default(0);
+            $table->decimal('unit_rate')->default(0);
+            $table->timestamps();
+
+            $table->index(['name', 'caretaker_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
